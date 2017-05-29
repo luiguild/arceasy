0ff80f25f8c76ebc5098ede79ba8f694d45faf26
Luigui Delyer <github@s1x.com.br>
2017-05-28 22:16:27 -0300

    inclusão dos comentários nas funções de log

--- src/logger.js
+++ src/logger.js
@@ -5,6 +5,12 @@ let errorCounter = 0
 let fatalCounter = 0
 let warnCounter = 0
 
+/**
+ * Add conter and prefix and execute simple console.log()
+ * @param  {String} message - Your log text
+ * @param  {Object} _object - If you pass an object or array, it's will be put together
+ * @return {Function}
+ */
 const log = (message, _object) => {
     const object = _object !== undefined
         ? _object
@@ -15,6 +21,12 @@ const log = (message, _object) => {
     return console.log(`${logCounter} ${prefix} ${message}`, object)
 }
 
+/**
+ * Add conter and prefix and execute console.error()
+ * @param  {String} message - Your log text
+ * @param  {Object} _object - If you pass an object or array, it's will be put together
+ * @return {Function}
+ */
 const error = (message, _object) => {
     const object = _object !== undefined
         ? _object
@@ -25,12 +37,23 @@ const error = (message, _object) => {
     return console.error(`${errorCounter} ${prefix} ${message}`, object)
 }
 
+/**
+ * Add conter and prefix and create a throw new Error()
+ * @param  {String} message - Your log text
+ * @return {Function}
+ */
 const fatal = (message) => {
     fatalCounter++
 
     throw new Error(`${fatalCounter} ${prefix} ${message}`)
 }
 
+/**
+ * Add conter and prefix and execute console.warn()
+ * @param  {String} message - Your log text
+ * @param  {Object} _object - If you pass an object or array, it's will be put together
+ * @return {Function}
+ */
 const warn = (message, _object) => {
     const object = _object !== undefined
         ? _object
